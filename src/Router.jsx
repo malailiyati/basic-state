c149b2aa21d8b3e93ec7a88493b26ac43374f78e
unknown <malailiyati107@gmail.com>
2025-08-07 09:15:28 +0700

    feat: add route for todoredux and survey page

--- src/Router.jsx
+++ src/Router.jsx
@@ -5,6 +5,8 @@ import { AuthContext } from "./component/contexts/authContext";
 import LoginForm from "./component/Form";
 import Header from "./component/Header";
 import TodoForm from "./component/Todo";
+import TodoRedux from "./component/todoRedux";
+import SurveyPage from "./component/SurveyPage";
 
 function Router() {
   return (
@@ -13,6 +15,8 @@ function Router() {
         <Route element={<RouteLayout />}>
           <Route path="/" element={<ShoppingApp />}></Route>
           <Route path="/todo" element={<TodoForm />} />
+          <Route path="/todoredux" element={<TodoRedux />} />
+          <Route path="/survey" element={<SurveyPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
